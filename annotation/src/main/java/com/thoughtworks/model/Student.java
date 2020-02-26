1e76d05d63c8648c231fd19871ec98e47c855d35
meishaaaaa <meishaaaaa@163.com>
2020-02-26 22:37:52 +0800

    change1

--- annotation/src/main/java/com/thoughtworks/model/Student.java
+++ annotation/src/main/java/com/thoughtworks/model/Student.java
@@ -1,9 +1,21 @@
 package com.thoughtworks.model;
 
+import java.lang.annotation.*;
+
 // 为Student类添加一个Alias注解, 表明Student类可以取一个自定义的别名
+@Student.Alias("可以自定义别名")
 public class Student {
 
     // 为age字段添加一个Limit注解, 表明age字段的最小值为1 最大值为140
+    @Limit(min = 1, max = 140)
     private int age;
 
+    public @interface Limit {
+        int min();
+        int max();
+    }
+
+    public @interface Alias {
+        String value() default "";
+    }
 }
